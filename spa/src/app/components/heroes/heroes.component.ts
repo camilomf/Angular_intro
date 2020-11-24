18194db0d83a73d22d733e43df820d7d2f324448
user <camilo.mino03@inacapmail.cl>
2020-11-24 16:21:01 -0300

    base

--- spa/src/app/components/heroes/heroes.component.ts
+++ spa/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService } from '../../servicios/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -11,13 +12,20 @@ export class HeroesComponent implements OnInit {
 
   heroes:any[] = [];
 
-  constructor( private _heroesService:HeroesService ) { 
+  constructor( 
+    private _heroesService:HeroesService,
+    private router: Router
+    ) { 
 
   }
 
   ngOnInit() {
     this.heroes= this._heroesService.getHeroes();
     // console.log(this.heroes);
+  }
+
+  verHeroe(idx : number){
+    this.router.navigate(['/heroe',idx]);
   }
 
 }
