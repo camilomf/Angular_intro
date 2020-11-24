4e591e28953a4cdb0372914f0b901124c35b700e
user <camilo.mino03@inacapmail.cl>
2020-11-24 15:49:39 -0300

    base

---
+++ spa/src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../servicios/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: [
+  ]
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:any[] = [];
+
+  constructor( private _heroesService:HeroesService ) { 
+
+  }
+
+  ngOnInit() {
+    this.heroes= this._heroesService.getHeroes();
+    // console.log(this.heroes);
+  }
+
+}
+ 
